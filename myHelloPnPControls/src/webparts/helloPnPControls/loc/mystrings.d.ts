036ab6dfd3c1f2f54fa1571f471b886335541b8b
wonusuh <wonwooseo23@gmail.com>
2024-07-29 10:31:09 +0900

    just the framework and some packages have been installed.

---
+++ myHelloPnPControls/src/webparts/helloPnPControls/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IHelloPnPControlsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'HelloPnPControlsWebPartStrings' {
+  const strings: IHelloPnPControlsWebPartStrings;
+  export = strings;
+}
