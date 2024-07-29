81d6d8ba895a07f08eb6ad9f88a318081ab45456
wonusuh <wonwooseo23@gmail.com>
2024-07-29 11:28:37 +0900

    second commit

--- testPnP/src/webparts/helloPnPContorls/HelloPnPContorlsWebPart.ts
+++ testPnP/src/webparts/helloPnPContorls/HelloPnPContorlsWebPart.ts
@@ -53,8 +53,6 @@ export default class HelloPnPContorlsWebPart extends BaseClientSideWebPart<IHell
     });
   }
 
-
-
   private _getEnvironmentMessage(): Promise<string> {
     if (!!this.context.sdks.microsoftTeams) { // running in Teams, office.com or Outlook
       return this.context.sdks.microsoftTeams.teamsJs.app.getContext()
