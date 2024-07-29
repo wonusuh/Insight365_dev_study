6a765aa4757247018503cfc9af41f1679b2229cf
wonusuh <wonwooseo23@gmail.com>
2024-07-29 14:57:33 +0900

    this commit is just done after gulp serve;

---
+++ mySPFxAadHttpClient/src/webparts/spFxAadHttpClient/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface ISpFxAadHttpClientWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'SpFxAadHttpClientWebPartStrings' {
+  const strings: ISpFxAadHttpClientWebPartStrings;
+  export = strings;
+}
